5180fc3cc1753ba25309dc2c0510a924ec6fc01f
smt-codes <smtcodes@gmail.com>
2020-10-16 15:23:25 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+const users = {};
+
+io.on('connection', socket => {
+    socket.on('new-user-join', user => {
+        users[socket.id] = user;
+        socket.broadcast.emit('user-joined', user);
+        console.log('ne user ' + user);
+    });
+
+    socket.on('send', message => {
+        socket.broadcast.emit('receive', { message: message, user: users[socket.id] });
+    });
+
+    socket.on('disconnect', message => {
+        socket.broadcast.emit('left', { message: message, user: users[socket.id] });
+        delete users[socket.id];
+    })
+})
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ public/script.js
@@ -0,0 +1,66 @@
+const socket = io();
+
+const txt = document.getElementById('txt_message');
+const container = document.getElementById('container');
+var audio = new Audio('audio.mp3');
+
+const user = prompt("Enter Your Name");
+socket.emit('new-user-join', user);
+
+const append = (sender, message, position) => {
+    messageElement = document.createElement('div');
+    messageElement.className = 'row pl-2 pr-2 mt-3 mb-3';
+    messageInnerElement = document.createElement('div');
+    if (sender != '') {
+        spanElement = document.createElement('span');
+        spanElement.className = 'text-secondary';
+        spanElement.append(sender);
+        messageInnerElement.append(spanElement);
+        messageInnerElement.append(document.createElement('br'));
+    }
+    messageInnerElement.append(message);
+    if (position == 'left') {
+        audio.play();
+        messageInnerElement.className = 'col-8 border rounded pt-3 pb-3';
+    } else {
+        messageInnerElement.className = 'col-8 offset-4 border rounded pt-3 pb-3';
+    }
+    messageElement.append(messageInnerElement);
+    container.append(messageElement);
+};
+
+const bottom = () => {
+    container.scrollTop = container.scrollHeight;
+};
+
+txt.onkeyup = (event) => {
+    if (event.keyCode === 13) {
+        document.getElementById('btn_send').click();
+    }
+};
+
+document.getElementById('btn_send').onclick = () => {
+    if (txt.value != '') {
+        var ms = txt.value;
+        socket.emit('send', ms);
+        append('You :', ms, '');
+        txt.value = '';
+        bottom();
+    }
+    txt.focus();
+};
+
+socket.on('user-joined', newuser => {
+    append('', newuser + ' Joined the Chat', 'left');
+    bottom();
+});
+
+socket.on('receive', data => {
+    append(data.user + " :", data.message, 'left');
+    bottom();
+});
+
+socket.on('left', data => {
+    append('', data.user + ' Left From Chat', 'left');
+    bottom();
+});
